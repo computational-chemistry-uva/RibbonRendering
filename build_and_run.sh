e8655195d41a95dccf90f541d0a33b0a24b0bc81
Mario Holubar <mario.holubar@googlemail.com>
2024-12-28 20:33:13 +0100

    update build tools

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
-#rm -f a.out
-#echo "Compiling..."
+PROJECT_NAME=$(basename $(pwd))
 
-#FLAGS="-Wall -g -std=c++20"
-#SOURCE_FILES="*.cpp"
-#time g++ $FLAGS $SOURCE_FILES -o a.out
-
-#if [ -f "a.out" ]; then
-#    time ./a.out
-#	#valgrind --log-file="valgrind.log" ./a.out
-#fi
-
-mkdir -p build && cd build && cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja .. --log-level=WARNING && cmake --build . && ./spline_rendering
+mkdir -p build && \
+cd build && \
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja --log-level=WARNING .. && \
+cmake --build . && \
+./$PROJECT_NAME
--- shell.nix
+++ shell.nix
@@ -1,39 +1,23 @@
-let
-  pkgs = import <nixpkgs> {};
-in pkgs.mkShell {
-  packages = with pkgs; [
-      cmake
-      clang-tools
-      gdb
-      libcxx
-      glfw
-      glew
-      libGL
-      stdenv.cc.cc.lib
-      imgui
-      glm
-      ninja
-  ];
+with import <nixpkgs> {};
 
-  shellHook = ''
-    # Debugger path
-    export GDB_PATH="${pkgs.gdb}/bin/gdb"
+pkgs.mkShell {
+    packages = [
+        #stdenv.cc.cc.lib
+        #libcxx
+        cmake
+        ninja
+        #clang # This ovverrides clang-tools' clangd and causes problems for LSP
+        clang-tools # For LSP
+        libGL
+        glfw
+        glew
+        glm
+    ];
 
-    export IMGUI_PATH="${pkgs.imgui}/include/imgui/"
+    nativeBuildInputs = [
+        #pkg-config
+    ];
 
-    # System headers
-    export CPLUS_INCLUDE_PATH="${pkgs.imgui}/include/imgui/:${pkgs.libcxx}/include/c++/v1:$CPLUS_INCLUDE_PATH"
-    export CPATH="${pkgs.gcc.libc}/include:${pkgs.libcxx}/include/c++/v1:$CPATH"
-
-    # Compiler resource dir (for system headers)
-    export CLANG_RESOURCE_DIR="$(clang -print-resource-dir)"
-
-    # Add OpenGL libs to path
-    export LD_LIBRARY_PATH="${pkgs.libGL}/lib:$LD_LIBRARY_PATH"
-  '';
-
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-    libcxx.dev
-  ];
+    shellHook = ''
+    '';
 }
