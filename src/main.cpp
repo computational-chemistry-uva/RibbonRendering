acb76b0d57035e9d879303fdfc01af3df1a9b510
Mario Holubar <34423543+mario-holubar@users.noreply.github.com>
2025-02-01 20:51:06 +0100

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,8 @@
 
 // NOTE This currently does not handle deletion of GL resources, they are kept alive until termination of the program.
 
-static void glfw_error_callback(int error, const char* description) {
-    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
-}
-
-void windowResizeCallback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
-
+// Struct to keep track of mouse input
+// d values indicate the change since the previous frame
 namespace global {
     static struct MouseState {
         float x = 0.0f;
@@ -26,6 +20,7 @@ namespace global {
         float dscroll = 0.0f;
         bool leftButtonDown = false;
 
+        // Update mouse position and change since last frame
         void update(GLFWwindow *window) {
             double x, y;
             glfwGetCursorPos(window, &x, &y);
@@ -33,40 +28,52 @@ namespace global {
             this->dy = float(y) - this->y;
             this->x = float(x);
             this->y = float(y);
-            this-> dscroll = this->scroll;
+            this->dscroll = this->scroll;
             this->scroll = 0.0f;
         }
     } mouse;
 };
 
+// Print GLFW errors
+static void glfw_error_callback(int error, const char* description) {
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+// Resize GL viewport on window resize
+void windowResizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+// Handle keyboard input
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    // Close window on Escape
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
-    if (ImGui::GetIO().WantCaptureKeyboard) return;
     }
+    // Ignore keypresses that are handled by ImGui
     if (ImGui::GetIO().WantCaptureKeyboard) return;
 }
 
+// Handle mouse input
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
     // Register mouse release even when over UI
     if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
         global::mouse.leftButtonDown = false;
-        // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
     }
     // Only register click when not over UI
     if (ImGui::GetIO().WantCaptureMouse) return;
     if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
         global::mouse.leftButtonDown = true;
-        // glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
     }
 }
-
 void mouseScrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
+    // Only register scroll when not over UI
     if (ImGui::GetIO().WantCaptureMouse) return;
     global::mouse.scroll += yoffset;
 }
 
 int main() {
+    // Print GLFW errors
     glfwSetErrorCallback(glfw_error_callback);
 
     // Initialize GLFW
@@ -78,7 +85,8 @@ int main() {
     const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_SAMPLES, 16); // MSAA
+    // Enable MSAA (works with impostor shaders out of the box!)
+    //glfwWindowHint(GLFW_SAMPLES, 16);
 
     // Create window
     GLFWwindow* window = glfwCreateWindow(1280, 720, "Viewer", NULL, NULL);
@@ -87,7 +95,9 @@ int main() {
         return 1;
     }
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable VSync
+
+    // Enable VSync
+    glfwSwapInterval(1);
 
     // Initialize GLEW
     if (glewInit() != GLEW_OK) {
@@ -95,22 +105,21 @@ int main() {
         return 1;
     }
 
-    // Depth testing
+    // Enable depth testing
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
-    // Backface culling
+    // Enable backface culling
     glEnable(GL_CULL_FACE);
-    // Alpha blending
+    // Enable alpha blending
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    // MSAA
-    //glEnable(GL_MULTISAMPLE);
 
     // Set callbacks
     glfwSetFramebufferSizeCallback(window, windowResizeCallback);
     glfwSetKeyCallback(window, keyCallback);
     glfwSetMouseButtonCallback(window, mouseButtonCallback);
     glfwSetScrollCallback(window, mouseScrollCallback);
+    windowResizeCallback(window, 1280, 720);
 
     // Imgui setup
     // NOTE Needs to be done after setting input callbacks!
@@ -123,59 +132,44 @@ int main() {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // Setup projection matrix
-    windowResizeCallback(window, 1280, 720);
-
-    std::vector<glm::vec3> points = {
-        glm::vec3(0.0f, 0.0f, 0.5f),
-        glm::vec3(0.0f, 0.0f, -0.5f),
-        glm::vec3(0.0f, 0.5f, -1.0f),
-    };
-
-    DrawObject mesh = createMesh(points);
-    DrawObject spheres = createSpheres(points);
-    DrawObject cylinders = createCylinders(points);
-
+    // Load and compile shaders
     GLuint meshWireframeProgram = createShaderProgram(
         "../src/mesh_vertex.glsl",
         "../src/wireframe_fragment.glsl"
     );
-
     GLuint meshProgram = createShaderProgram(
         "../src/mesh_vertex.glsl",
         "../src/mesh_fragment.glsl"
     );
-
     GLuint sphereWireframeProgram = createShaderProgram(
         "../src/sphere_vertex.glsl",
         "../src/wireframe_fragment.glsl"
     );
-
     GLuint sphereProgram = createShaderProgram(
         "../src/sphere_vertex.glsl",
         "../src/sphere_fragment.glsl"
     );
-
     GLuint cylinderWireframeProgram = createShaderProgram(
         "../src/cylinder_vertex.glsl",
         "../src/wireframe_fragment.glsl"
     );
-
     GLuint cylinderProgram = createShaderProgram(
         "../src/cylinder_vertex.glsl",
         "../src/cylinder_fragment.glsl"
     );
 
-    Uniforms uniforms;
+    // Create Objects
+    std::vector<glm::vec3> points = {
+        glm::vec3(0.0f, 0.0f, 0.5f),
+        glm::vec3(0.0f, 0.0f, -0.5f),
+        glm::vec3(0.0f, 0.5f, -1.0f),
+    };
+    DrawObject mesh = createMesh(points);
+    DrawObject spheres = createSpheres(points);
+    DrawObject cylinders = createCylinders(points);
 
-    float yaw = -45.0f;
-    float pitch = 30.0f;
-    float dist = 5.0f;
-    float fov = 45.0f;
-    bool drawWireframes = true;
-    bool drawMesh = false;
-    bool drawSpheres = false;
-    bool drawCylinders = true;
+    // Set default parameters
+    Uniforms uniforms;
     uniforms.drawNormals = false;
     uniforms.lightIntensity = 1.0f;
     uniforms.sphereRadius = 0.25f;
@@ -184,77 +178,104 @@ int main() {
     uniforms.cylinderMode = 2;
     uniforms.pitch = 0.5f;
     uniforms.width = 0.25f;
+    float camYaw = -45.0f;
+    float camPitch = 30.0f;
+    float camDist = 5.0f;
+    float fov = 45.0f;
+    bool drawWireframes = true;
+    bool drawMesh = false;
+    bool drawSpheres = false;
+    bool drawCylinders = true;
 
     // Main loop
     while (!glfwWindowShouldClose(window)) {
-        // Get user input and window events
+        // Handle events
         glfwPollEvents();
+
+        // Update mouse state
         global::mouse.update(window);
 
+        // UI
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-
         ImGui::SetNextWindowPos({0, 0});
         ImGui::Begin("Debug", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize);
+        {
+            // Toggle visibility of objects
+            ImGui::Text("Objects");
+            ImGui::Indent();
+            {
+                ImGui::Checkbox("Mesh", &drawMesh);
+                ImGui::Checkbox("Spheres", &drawSpheres);
+                ImGui::Checkbox("Cylinders", &drawCylinders);
+            }
+            ImGui::Unindent();
 
-        ImGui::Text("Objects");
-        ImGui::Indent();
-        ImGui::Checkbox("Mesh", &drawMesh);
-        ImGui::Checkbox("Spheres", &drawSpheres);
-        ImGui::Checkbox("Cylinders", &drawCylinders);
-        ImGui::Unindent();
+            // Settings that apply to all objects
+            ImGui::Spacing();
+            ImGui::Text("Draw settings");
+            ImGui::Indent();
+            {
+                ImGui::Checkbox("Draw wireframes", &drawWireframes);
+                ImGui::Checkbox("Draw normals", &uniforms.drawNormals);
+                ImGui::SetNextItemWidth(128);
+                if (uniforms.drawNormals) ImGui::BeginDisabled();
+                {
+                    ImGui::SliderFloat("Light", &uniforms.lightIntensity, 0.0f, 2.0f, "%.2f", ImGuiSliderFlags_NoRoundToFormat);
+                }
+                if (uniforms.drawNormals) ImGui::EndDisabled();
+            }
+            ImGui::Unindent();
 
-        ImGui::Spacing();
-        ImGui::Text("Draw settings");
-        ImGui::Indent();
-        ImGui::Checkbox("Draw wireframes", &drawWireframes);
-        ImGui::Checkbox("Draw normals", &uniforms.drawNormals);
-        ImGui::SetNextItemWidth(128);
-        if (uniforms.drawNormals) ImGui::BeginDisabled();
-        ImGui::SliderFloat("Light", &uniforms.lightIntensity, 0.0f, 2.0f, "%.2f", ImGuiSliderFlags_NoRoundToFormat);
-        if (uniforms.drawNormals) ImGui::EndDisabled();
-        ImGui::Unindent();
+            // Spheres parameters
+            if (!drawSpheres) ImGui::BeginDisabled();
+            {
+                ImGui::Spacing();
+                ImGui::Text("Sphere impostors");
+                ImGui::Indent();
+                {
+                    ImGui::SetNextItemWidth(128);
+                    ImGui::SliderFloat("Radius##Sphere", &uniforms.sphereRadius, 0.1f, 1.0f, "%.2f", ImGuiSliderFlags_NoRoundToFormat);
+                    ImGui::Checkbox("Ray traced", &uniforms.raytraced);
+                }
+                ImGui::Unindent();
+            }
+            if (!drawSpheres) ImGui::EndDisabled();
 
-        if (!drawSpheres) ImGui::BeginDisabled();
-        ImGui::Spacing();
-        ImGui::Text("Sphere impostors");
-        ImGui::Indent();
-        ImGui::SetNextItemWidth(128);
-        ImGui::SliderFloat("Radius##Sphere", &uniforms.sphereRadius, 0.1f, 1.0f, "%.2f", ImGuiSliderFlags_NoRoundToFormat);
-        ImGui::Checkbox("Ray traced", &uniforms.raytraced);
-        ImGui::Unindent();
-        if (!drawSpheres) ImGui::EndDisabled();
-
-        if (!drawCylinders) ImGui::BeginDisabled();
-        ImGui::Spacing();
-        ImGui::Text("Cylinder impostors");
-        ImGui::Indent();
-        ImGui::SetNextItemWidth(128);
-        ImGui::SliderFloat("Radius##Cylinder", &uniforms.cylinderRadius, 0.05f, 0.25f, "%.2f", ImGuiSliderFlags_NoRoundToFormat);
-        ImGui::SetNextItemWidth(128);
-        ImGui::Combo("Mode", &uniforms.cylinderMode, "Simple\0Rounded\0Ribbon\0");
-        if (uniforms.cylinderMode != 2) ImGui::BeginDisabled();
-        ImGui::SetNextItemWidth(128);
-        ImGui::SliderFloat("Pitch", &uniforms.pitch, 0.05f, 1.0f, "%.2f", ImGuiSliderFlags_NoRoundToFormat);
-        ImGui::SetNextItemWidth(128);
-        ImGui::SliderFloat("Width", &uniforms.width, 0.05f, 1.0f, "%.2f", ImGuiSliderFlags_NoRoundToFormat);
-        if (uniforms.cylinderMode != 2) ImGui::EndDisabled();
-        ImGui::Unindent();
-        if (!drawCylinders) ImGui::EndDisabled();
-
+            // Cylinders parameters
+            if (!drawCylinders) ImGui::BeginDisabled();
+            ImGui::Spacing();
+            ImGui::Text("Cylinder impostors");
+            ImGui::Indent();
+            {
+                ImGui::SetNextItemWidth(128);
+                ImGui::SliderFloat("Radius##Cylinder", &uniforms.cylinderRadius, 0.05f, 0.25f, "%.2f", ImGuiSliderFlags_NoRoundToFormat);
+                ImGui::SetNextItemWidth(128);
+                ImGui::Combo("Mode", &uniforms.cylinderMode, "Simple\0Rounded\0Ribbon\0");
+                if (uniforms.cylinderMode != 2) ImGui::BeginDisabled();
+                ImGui::SetNextItemWidth(128);
+                ImGui::SliderFloat("Pitch", &uniforms.pitch, 0.05f, 1.0f, "%.2f", ImGuiSliderFlags_NoRoundToFormat);
+                ImGui::SetNextItemWidth(128);
+                ImGui::SliderFloat("Width", &uniforms.width, 0.05f, 1.0f, "%.2f", ImGuiSliderFlags_NoRoundToFormat);
+                if (uniforms.cylinderMode != 2) ImGui::EndDisabled();
+            }
+            ImGui::Unindent();
+            if (!drawCylinders) ImGui::EndDisabled();
+        }
         ImGui::End();
 
+        // Update camera parameters based on mouse state
         if (global::mouse.leftButtonDown) {
-            yaw += global::mouse.dx * 0.2f;
-            pitch += global::mouse.dy * 0.2f;
-            pitch = glm::clamp(pitch, -90.0f, 90.0f);
-            yaw = fmod(yaw + 360.0f, 360.0f);
+            camYaw += global::mouse.dx * 0.2f;
+            camPitch += global::mouse.dy * 0.2f;
+            camPitch = glm::clamp(camPitch, -90.0f, 90.0f);
+            camYaw = fmod(camYaw + 360.0f, 360.0f);
         }
-        dist = (1.0f - 0.25f * global::mouse.dscroll) * dist;
-        dist = glm::clamp(dist, 1.0f, 10.0f);
+        camDist = (1.0f - 0.25f * global::mouse.dscroll) * camDist;
+        camDist = glm::clamp(camDist, 1.0f, 10.0f);
 
-        // Projection matrix
+        // Create projection matrix
         int w, h;
         glfwGetWindowSize(window, &w, &h);
         uniforms.projection = glm::perspective(
@@ -263,15 +284,15 @@ int main() {
             0.1f,                // Near plane
             100.0f               // Far plane
         );
-        // View matrix
+        // Create view matrix
         uniforms.view = glm::mat4(1.0f);
-        uniforms.view = glm::translate(uniforms.view, glm::vec3(0.0f, 0.0f, -dist));
-        uniforms.view = glm::rotate(uniforms.view, glm::radians(pitch), glm::vec3(1.0f, 0.0f, 0.0f));
-        uniforms.view = glm::rotate(uniforms.view, glm::radians(yaw), glm::vec3(0.0f, 1.0f, 0.0f));
-        // Model matrix
+        uniforms.view = glm::translate(uniforms.view, glm::vec3(0.0f, 0.0f, -camDist));
+        uniforms.view = glm::rotate(uniforms.view, glm::radians(camPitch), glm::vec3(1.0f, 0.0f, 0.0f));
+        uniforms.view = glm::rotate(uniforms.view, glm::radians(camYaw), glm::vec3(0.0f, 1.0f, 0.0f));
+        // Create model matrix
         uniforms.model = glm::mat4(1.0f);
 
-        // Camera and light positions
+        // Set camera and light position uniforms
         uniforms.viewPos = glm::inverse(uniforms.view) * glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
         uniforms.lightPos = glm::vec3(3.0f, 3.5f, 2.5f);
 
@@ -285,13 +306,15 @@ int main() {
         if (drawSpheres) draw(spheres, sphereProgram, uniforms);
         if (drawCylinders) draw(cylinders, cylinderProgram, uniforms);
         if (drawWireframes) {
+            // Draw wireframes with no backface culling and on top of everything else
             glDisable(GL_CULL_FACE);
-            glDepthRange(0.0, 0.01);
+            glDepthRange(0.0, 0.0);
             if (drawMesh) draw(mesh, meshWireframeProgram, uniforms);
             if (drawSpheres) draw(spheres, sphereWireframeProgram, uniforms);
             if (drawCylinders) draw(cylinders, cylinderWireframeProgram, uniforms);
         }
 
+        // Draw UI
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
